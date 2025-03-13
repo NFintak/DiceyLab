4d3ede2e4561407800351ee83c6561c890b22c7d
Nathan <natefintak@gmail.com>
2025-03-12 22:20:55 -0400

    in progress

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -7,6 +7,10 @@ public class Bins {
         this.minNum = minNum;
         this.maxNum = maxNum;
     }
+    //make containers that span from min possible num to max possible num, then -
+    //take input and sort it into the container that matches the input
+    //if the result of a 2 die roll is 3, add the result to the bin for 3
+    //if the result is 8, add it to the bin for 8, etc.
 
 
 }
