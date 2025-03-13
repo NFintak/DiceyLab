841affd304e1c502518f8fba71e72e114c6ec5a5
Nathan <natefintak@gmail.com>
2025-03-13 16:38:15 -0400

    in progress

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -2,15 +2,24 @@
 public class Bins {
     private Integer minNum; //minimum number for the bin
     private Integer maxNum; //maximum number for the bin
+    //private Integer[][] histogram; //may need to look into hashmaps more for this one
 
     public Bins(Integer minNum, Integer maxNum) {
         this.minNum = minNum;
         this.maxNum = maxNum;
+        //this.histogram = histogram;
     }
     //make containers that span from min possible num to max possible num, then -
     //take input and sort it into the container that matches the input
     //if the result of a 2 die roll is 3, add the result to the bin for 3
     //if the result is 8, add it to the bin for 8, etc.
+    public Integer[][] getHistogramResults(Integer minNum) {
+        //for (int i = minNum; i < histogram.length; i++) {
+
+        //}
+        //return this.histogram;
+        return null;
+    }
 
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,21 +1,33 @@
+
+
 public class Simulation {
     private Integer numOfThrows;
     private Integer sumOfThrows;
+    private Integer numOfDice;
+    //private ArrayList<Integer> diceResults = new ArrayList<>();
 
     public Simulation(Integer diceToRoll, Integer numOfThrows) {
         Dice dice = new Dice(diceToRoll);
+        Bins bins = new Bins(diceToRoll, diceToRoll * 6);
+        this.numOfDice = diceToRoll;
         this.numOfThrows = numOfThrows;
         this.sumOfThrows = 0;
     }
 
     public Integer runSimulation(Dice dice, Integer numOfThrows) {
         for (int i = 0; i < numOfThrows; i++) {
-            sumOfThrows += dice.tossAndSum();
+            Integer currentThrow = dice.tossAndSum();
+            sumOfThrows += currentThrow;
+            //diceResults.add(currentThrow);
         }
         return sumOfThrows;
     }
 
     public void printResults() {
+        System.out.println("***\n");
+        System.out.println("Simulation of " + numOfDice + " dice tossed for " + numOfThrows + " times.\n");
+        System.out.println("***\n");
+
         //need to work on bins class before adding this in
     }
 
